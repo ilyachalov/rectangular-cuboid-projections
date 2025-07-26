2dce7a530072b818c2a160b25957ecc10fb62f9a
Ilya Chalov <48008056+ilyachalov@users.noreply.github.com>
2025-07-26 14:05:23 +0300

    Add files via upload

--- methods/DrawRectCuboid_Dimetric.cs
+++ methods/DrawRectCuboid_Dimetric.cs
@@ -1,24 +1,24 @@
-// рисование прямоугольного параллелепипеда в прямоугольной изометрической проекции
+// рисование прямоугольного параллелепипеда в прямоугольной диметрической проекции
 //   x0, y0 - оконные координаты начала объемной системы координат
 //            (невидимая вершина прямоугольного параллелепипеда)
 //   sizeX, sizeY, sizeZ - размеры параллелепипеда по осям объемной системы координат
 //   mathStrict - математически точные (true) или приведенные (false) коэффициенты
 //                искажения
-void DrawRectCuboid_Isometric(Graphics g, int x0, int y0, int sizeX, int sizeY, int sizeZ,
-                              bool mathStrict = false)
+private void DrawRectCuboid_Dimetric(Graphics g, int x0, int y0, int sizeX, int sizeY, int sizeZ,
+                                     bool mathStrict = false)
 {
     // Предполагаем, что в параметры sizeX, sizeY и sizeZ переданы реальные размеры
-    // фигуры (параллелепипеда). В математически точной прямоугольной изометрической
+    // фигуры (параллелепипеда). В математически точной прямоугольной диметрической
     // проекции эти размеры уменьшатся. Однако, в российском стандарте ГОСТ 2.317-2011
     // сказано, что в данной проекции, как правило, коэффициенты искажения округляют.
 
     // математически точные коэффициенты искажения
-    double distCoefX = 0.82;
-    double distCoefY = 0.82;
-    double distCoefZ = 0.82;
+    double distCoefX = 0.94;
+    double distCoefY = 0.47;
+    double distCoefZ = 0.94;
     // приведенные (округленные) коэффициенты искажения
     // double rDistCoefX = 1.0;
-    // double rDistCoefY = 1.0;
+    double rDistCoefY = 0.5;
     // double rDistCoefZ = 1.0;
 
     // применение коэффициентов искажения к размерам фигуры (параллелепипеда)
@@ -32,18 +32,20 @@ void DrawRectCuboid_Isometric(Graphics g, int x0, int y0, int sizeX, int sizeY, 
     else
     {
         dSizeX = sizeX;
-        dSizeY = sizeY;
+        dSizeY = (int)(sizeY * rDistCoefY);
         dSizeZ = sizeZ;
     }
 
     // расчет синусов углов (градусы переведены в радианы)
-    double sin60 = Math.Sin(Math.PI * 60.0 / 180.0);
-    double sin30 = Math.Sin(Math.PI * 30.0 / 180.0);
+    double sin7_10  = Math.Sin(Math.PI * (43 / 6) / 180.0);   // синус угла  7°10′
+    double sin82_50 = Math.Sin(Math.PI * (497 / 6) / 180.0);  // синус угла 82°50′
+    double sin41_25 = Math.Sin(Math.PI * (497 / 12) / 180.0); // синус угла 41°25′
+    double sin48_35 = Math.Sin(Math.PI * (583 / 12) / 180.0); // синус угла 48°35′
 
     // семь видимых вершин прямоугольного параллелепипеда
-    Point pX = new Point(x0 - (int)(sin60 * dSizeX), y0 + (int)(sin30 * dSizeX));
-    Point pY = new Point(x0 + (int)(sin60 * dSizeY), y0 + (int)(sin30 * dSizeY));
-    Point pXY = new Point(pX.X + (int)(sin60 * dSizeY), pX.Y + (int)(sin30 * dSizeY));
+    Point pX = new Point(x0 - (int)(sin82_50 * dSizeX), y0 + (int)(sin7_10 * dSizeX));
+    Point pY = new Point(x0 + (int)(sin48_35 * dSizeY), y0 + (int)(sin41_25 * dSizeY));
+    Point pXY = new Point(pX.X + (int)(sin48_35 * dSizeY), pX.Y + (int)(sin41_25 * dSizeY));
     Point pZ = new Point(x0, y0 - dSizeZ);
     Point pZX = new Point(pX.X, pX.Y - dSizeZ);
     Point pZY = new Point(pY.X, pY.Y - dSizeZ);
@@ -54,10 +56,10 @@ void DrawRectCuboid_Isometric(Graphics g, int x0, int y0, int sizeX, int sizeY, 
     // искажения, и не меняются при снятии/установке чекбокса, чтобы не отвлекать
     // от изменения размеров самой фигуры)
     int axisAdd = 30; // насколько ось будет длиннее размера фигуры
-    Point pAxisX = new Point(x0 - (int)(sin60 * (sizeX + axisAdd)),
-                             y0 + (int)(sin30 * (sizeX + axisAdd)));
-    Point pAxisY = new Point(x0 + (int)(sin60 * (sizeY + axisAdd)),
-                             y0 + (int)(sin30 * (sizeY + axisAdd)));
+    Point pAxisX = new Point(x0 - (int)(sin82_50 * (sizeX + axisAdd)),
+                             y0 + (int)(sin7_10 * (sizeX + axisAdd)));
+    Point pAxisY = new Point(x0 + (int)(sin48_35 * (sizeY + axisAdd) * rDistCoefY),
+                             y0 + (int)(sin41_25 * (sizeY + axisAdd) * rDistCoefY));
     Point pAxisZ = new Point(x0, y0 - (sizeZ + axisAdd));
 
     // рисование осей координат
